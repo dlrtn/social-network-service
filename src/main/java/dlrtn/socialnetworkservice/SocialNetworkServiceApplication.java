43f637370e7a1434bcb5566129bfa34ea88a1344
dlrtn <dlrtn751@gmail.com>
2022-08-10 18:49:59 +0900

    외부 설정 및 초기 설정 작성

---
+++ src/main/java/dlrtn/socialnetworkservice/SocialNetworkServiceApplication.java
@@ -0,0 +1,13 @@
+package dlrtn.socialnetworkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkServiceApplication.class, args);
+	}
+
+}
