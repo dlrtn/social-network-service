eead48dffa42652c9e41f2a8347e86320445661c
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-08-30 00:58:57 +0900

    Repository issue

--- src/main/java/dlrtn/socialnetworkservice/business/user/repository/UserRepository.java
+++ src/main/java/dlrtn/socialnetworkservice/business/user/repository/UserRepository.java
@@ -4,7 +4,7 @@ import dlrtn.socialnetworkservice.business.user.model.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    //todo Repository interface 변경
     boolean existsByUsername(String username);
 
 }
