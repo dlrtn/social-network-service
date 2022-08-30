c67a785e9e04e810df7c2fb4f2e7777492fa3664
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-08-30 23:29:15 +0900

    Test Code 작성 및 멱등성 보장을 위한 Annotation 추가

--- src/test/java/dlrtn/socialnetworkservice/business/user/application/UserServiceTests.java
+++ src/test/java/dlrtn/socialnetworkservice/business/user/application/UserServiceTests.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class UserServiceTests {
 
     @Autowired
@@ -21,6 +23,16 @@ public class UserServiceTests {
         SignUpResponse response = userService.signUp(request);
 
         Assertions.assertEquals("User sign up success", response.getDescription());
+    }
+
+    @Test
+    public void duplicateUserTest() {
+        SignUpRequest request = UserTestsConstants.TEST_SIGN_UP_REQUEST;
+
+        SignUpResponse response = userService.signUp(request);
+        SignUpResponse secondResponse = userService.signUp(request);
+
+        Assertions.assertEquals("The user id already exists", secondResponse.getDescription());
     }
 
 }
