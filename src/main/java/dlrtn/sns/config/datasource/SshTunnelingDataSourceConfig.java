7a6ba17ee7940327ef089baa1644451226797e4c
dlrtn <dlrtn751@gmail.com>
2024-05-23 18:43:58 +0900

    refactor(dataSource): 데이터베이스 터널링 연결 리팩토링 - 주석 추가 - 데이터베이스 호스트 포워딩 로직 수정 - 세션 연결과 터널링 생성 로직 분리 - mweide 의존성 추가

--- src/main/java/dlrtn/sns/config/datasource/SshTunnelingDataSourceConfig.java
+++ src/main/java/dlrtn/sns/config/datasource/SshTunnelingDataSourceConfig.java
@@ -1,6 +1,6 @@
-package dlrtn.sns.config.data_source;
+package dlrtn.sns.config.datasource;
 
-
+import com.jcraft.jsch.JSchException;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,22 +11,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
+/**
+ * 로컬 환경에서 데이터베이스 이용을 위한 SSH 터널링 데이터 소스 설정 클래스.
+ */
 @Slf4j
 @Profile("local")
 @Configuration
 @RequiredArgsConstructor
-public class SshDataSourceConfig {
+public class SshTunnelingDataSourceConfig {
 
     private final SshTunnelingInitializer initializer;
 
+    /**
+     * SSH 터널링을 통해 데이터 소스를 생성하고 구성합니다.
+     *
+     * @param properties 애플리케이션에 설정된 데이터 소스 속성.
+     * @return 구성된 데이터 소스.
+     * @throws JSchException SSH 세션 연결 및 터널링 중 오류가 발생한 경우.
+     */
     @Bean("dataSource")
     @Primary
-    public DataSource dataSource(DataSourceProperties properties) {
+    public DataSource dataSource(DataSourceProperties properties) throws JSchException {
+        initializer.buildSession();
+        Integer forwardedPort = initializer.buildSshTunnel();
 
-        Integer forwardedPort = initializer.buildSshTunnel();  // ssh 연결 및 터널링 설정
         String url = properties.getUrl()
                 .replace("[forwardedPort]", Integer.toString(forwardedPort));
-        log.info(url);
+        log.info("포워딩된 포트가 포함된 데이터베이스 URL: {}", url);
+
         return DataSourceBuilder.create()
                 .url(url)
                 .username(properties.getUsername())
@@ -34,5 +46,4 @@ public class SshDataSourceConfig {
                 .driverClassName(properties.getDriverClassName())
                 .build();
     }
-
 }
---
+++ src/main/java/dlrtn/sns/config/datasource/SshTunnelingInitializer.java
@@ -0,0 +1,87 @@
+package dlrtn.sns.config.datasource;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import jakarta.annotation.PreDestroy;
+import java.util.Properties;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * 내부 네트워크에서만 접속 가능한 원격 데이터베이스에 터널링하기 위한 클래스
+ * SSH 연결을 위해 JSch를 포크한 mweide를 사용(JSch 라이브러리의 제약사항이 많아 선택)
+ */
+@Slf4j
+@Profile("local")
+@Component
+@ConfigurationProperties(prefix = "ssh")
+@Setter
+public class SshTunnelingInitializer {
+
+    private String sshHost;
+    private int sshPort;
+    private String sshUsername;
+    private String privateKey;
+    private String passPhrase;
+
+    private int remoteDatabasePort;
+    private String remoteDatabaseHost;
+
+    private Session session;
+
+    /**
+     * 애플리케이션 컨텍스트가 종료될 때 SSH 세션을 닫습니다.
+     */
+    @PreDestroy
+    public void close() {
+        if (session != null && session.isConnected()) {
+            session.disconnect();
+            log.info("SSH 세션이 종료되었습니다");
+        }
+        log.info("SSH 세션이 연결되지 않았습니다");
+    }
+
+    /**
+     * 포트 포워딩을 설정하여 SSH 터널을 생성합니다.
+     *
+     * @return 원격 데이터베이스 포트로 포워딩된 로컬 포트.
+     * @throws JSchException SSH 세션 설정 또는 터널링 시 오류가 발생한 경우.
+     */
+    public Integer buildSshTunnel() throws JSchException {
+        try {
+            int forwardedPort = session.setPortForwardingL(0, remoteDatabaseHost,
+                    remoteDatabasePort);
+
+            log.info("SSH 터널이 로컬 포트 {}에 설정되었습니다", forwardedPort);
+            return forwardedPort;
+        } catch (JSchException e) {
+            log.error("SSH 세션 설정 실패", e);
+            this.close();
+            throw e;
+        }
+    }
+
+    public void buildSession() throws JSchException {
+        try {
+            JSch jSch = new JSch();
+            jSch.addIdentity(privateKey, passPhrase);
+            session = jSch.getSession(sshUsername, sshHost, sshPort);
+
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+
+            session.connect();
+
+            log.info("SSH 세션이 연결되었습니다");
+        } catch (JSchException e) {
+            log.error("SSH 세션 설정 실패", e);
+            this.close();
+            throw e;
+        }
+    }
+}
