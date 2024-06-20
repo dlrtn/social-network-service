47c1320c22e65b0900d9198efa1fb69904898a1f
dlrtn <dlrtn751@gmail.com>
2024-06-20 14:53:27 +0900

    docs(user): Swagger API 작업 및 불필요 EndPoint 제거

--- src/main/java/dlrtn/sns/business/user/controller/UserController.java
+++ src/main/java/dlrtn/sns/business/user/controller/UserController.java
@@ -3,27 +3,23 @@ package dlrtn.sns.business.user.controller;
 import dlrtn.sns.business.user.domain.User;
 import dlrtn.sns.business.user.domain.request.CreateUserRequest;
 import dlrtn.sns.business.user.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 @RequestMapping("/users")
+@Tag(name = "users", description = "사용자 관리 API")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("")
-    public String getUsers() {
-        return "users";
-    }
-
     @PostMapping("")
-    public User createUser(@RequestBody CreateUserRequest request) {
+    public User create(@RequestBody CreateUserRequest request) {
         return userService.save(request);
     }
 
--- src/main/java/dlrtn/sns/business/user/domain/request/CreateUserRequest.java
+++ src/main/java/dlrtn/sns/business/user/domain/request/CreateUserRequest.java
@@ -1,5 +1,7 @@
 package dlrtn.sns.business.user.domain.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,10 +9,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CreateUserRequest {
 
+    @Schema(description = "사용자 이메일", example = "test@test.com")
+    @Email
     private final String email;
 
+    @Schema(description = "사용자 비밀번호", example = "password")
     private final String password;
 
+    @Schema(description = "사용자 이름", example = "홍길동")
     private final String name;
+
+    @Schema(description = "사용자 전화번호", example = "010-1234-5678")
+    private final String phoneNumber;
 
 }
