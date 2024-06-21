4e65b0709ae120aea9c4d12b2ee45a43fa4c3440
dlrtn <dlrtn751@gmail.com>
2024-06-21 16:35:30 +0900

    update(user): User 엔티티 수정
    - email unique 제약 조건 추가
    - PK 타입 변경

--- src/main/java/dlrtn/sns/business/user/infrastructure/UserEntity.java
+++ src/main/java/dlrtn/sns/business/user/infrastructure/UserEntity.java
@@ -6,25 +6,25 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user", uniqueConstraints = {
+        @UniqueConstraint(name = "email_unique_key", columnNames = "email")})
 @Getter
 @NoArgsConstructor
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "BINARY(16)")
     private UUID id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
     @Column
@@ -37,6 +37,7 @@ public class UserEntity {
     private UserRole role;
 
     public UserEntity(User user) {
+        this.id = user.getId();
         this.email = user.getEmail();
         this.password = user.getPassword();
         this.name = user.getName();
