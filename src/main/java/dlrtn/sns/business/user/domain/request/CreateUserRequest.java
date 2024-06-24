4a67c9429550eaf69d909ec553cc228607fda2f4
dlrtn <dlrtn751@gmail.com>
2024-06-24 18:54:37 +0900

    feat(user): User Dto 검증 로직 추가
    - CreateUserRequest Dto 수정

--- src/main/java/dlrtn/sns/business/user/domain/request/CreateUserRequest.java
+++ src/main/java/dlrtn/sns/business/user/domain/request/CreateUserRequest.java
@@ -1,7 +1,8 @@
 package dlrtn.sns.business.user.domain.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,17 +10,19 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CreateUserRequest {
 
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Schema(description = "사용자 이메일", example = "test@test.com")
-    @Email
     private final String email;
 
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대,소문자, 숫자, 특수문자를 사용하세요.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     @Schema(description = "사용자 비밀번호", example = "password")
     private final String password;
 
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "사용자 이름은 2~10자의 한글만 사용가능합니다.")
+    @NotBlank(message = "사용자 이름은 필수 입력 값입니다.")
     @Schema(description = "사용자 이름", example = "홍길동")
     private final String name;
-
-    @Schema(description = "사용자 전화번호", example = "010-1234-5678")
-    private final String phoneNumber;
 
 }
